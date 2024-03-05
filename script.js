89b93fa057aaa750fede0c6e7d9daec5639a45bf
MichaEIL0 <dkujoo@gmail.com>
2024-03-05 15:32:38 +0100

    Add function that play one round of the game

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function GetComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
 
     return options[Math.floor(Math.random() * 3)];
+}
+
+function SingleRound(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection.toLowerCase() == "rock" && computerSelection == "paper":
+            return "AI wins. Paper beats rock!";
+            break;
+        case playerSelection.toLowerCase() == "rock" && computerSelection == "scissors":
+            return "Player wins. Rock beats scissors!";
+            break;
+        case playerSelection.toLowerCase() == "paper" && computerSelection == "rock":
+            return "Player wins. Paper beats rock!";
+            break;
+        case playerSelection.toLowerCase() == "paper" && computerSelection == "scissors":
+            return "AI wins. Scissors beats paper";
+            break;
+        case playerSelection.toLowerCase() == "scissors" && computerSelection == "rock":
+            return "AI wins. Rock beats scissors!";
+            break;    
+        case playerSelection.toLowerCase() == "scissors" && computerSelection == "paper":
+            return "Player wins. Scissors beats paper!";
+            break;     
+        case playerSelection.toLowerCase() == computerSelection:
+            return "There's a TIE!";
+            break;
+        default:
+            break;
+    }
+}
+
+function PlayGame() {
+    
 }
